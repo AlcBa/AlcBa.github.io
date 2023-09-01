d0aeacac74e07cf9fbb23491522c45e37016ab62
Lester Ong <lesterong776@gmail.com>
2023-09-01 13:17:56 +0800

    Initialise project structure

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,21 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      currentColor: 'currentColor',
+      white: '#ffffff',
+      'gray-100': '#212121',
+      'gray-200': '#404040',
+      'gray-300': '#808080',
+      'gray-400': '#cccccc',
+      'gray-500': '#fafafa',
+      'blue-100': '#00103d',
+      'blue-200': '#0a1a48',
+      'blue-300': '#133490',
+      'blue-400': '#8399d8',
+      'blue-500': '#d0d9f0',
+    },
     extend: {},
   },
   plugins: [],
