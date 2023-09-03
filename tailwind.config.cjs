cd193e0ce7ccd79ee0e881d6bd1a881992aae1e6
Lester Ong <lesterong776@gmail.com>
2023-09-03 15:04:52 +0800

    Update font to Open Sans

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -16,7 +18,11 @@ module.exports = {
       'blue-400': '#8399d8',
       'blue-500': '#d0d9f0',
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Open Sans Variable', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
