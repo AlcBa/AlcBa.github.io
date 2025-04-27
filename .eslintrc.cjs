202cc46c38dfa75d21d66343348a8310b0034273
lesterong <84223259+lesterong@users.noreply.github.com>
2025-04-27 15:13:13 +0800

    Upgrade to Tailwind 4 (#2)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   parser: '@typescript-eslint/parser',
   extends: ['eslint:recommended', 'plugin:astro/recommended', 'prettier'],
